10bf59595f945dc6c81b6ce2a2473e802c99aeac
leoneberisha <leonaberiisha@gmail.com>
2025-07-21 21:47:46 +0200

    .

--- LogIn/Form3.cs
+++ LogIn/Form3.cs
@@ -122,6 +122,24 @@ namespace LogIn
             Form4 form4 = new Form4();
             form4.ShowDialog();
         }
+
+        private void autoretToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form5 form5 = new Form5();
+            form5.ShowDialog();
+        }
+
+        private void kategoriteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form6 form6 = new Form6();  
+            form6.ShowDialog();
+        }
+
+        private void libraAktiveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form7 form7 = new Form7();  
+            form7.ShowDialog();
+        }
     }
     } 
 
