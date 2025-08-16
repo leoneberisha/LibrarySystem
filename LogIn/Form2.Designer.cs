770fdecd639a9ccc1395ccdd1b859a0b3e65564b
leoneberisha <leonaberiisha@gmail.com>
2025-08-16 19:38:52 +0200

    .

--- LogIn/Form2.Designer.cs
+++ LogIn/Form2.Designer.cs
@@ -349,6 +349,7 @@
             this.button1.TabIndex = 20;
             this.button1.Text = "Kerko";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // textBox4
             // 
--- LogIn/Form2.cs
+++ LogIn/Form2.cs
@@ -135,7 +135,9 @@ namespace LogIn
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+       
+
+        private void button1_Click_1(object sender, EventArgs e)
         {
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
