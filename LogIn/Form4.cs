2374efa252f54cab39e6eea50b788e79fcb4b202
leoneberisha <leonaberiisha@gmail.com>
2025-08-25 21:46:04 +0200

    .

--- LogIn/Form4.cs
+++ LogIn/Form4.cs
@@ -12,6 +12,19 @@ namespace LogIn
 {
     public partial class Form4 : Form
     {
+        string connectionString = "server=localhost; database=biblotek;uid=root;pwd=''";
+
+        private void Loadbibloteka()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = " SELECT id,id_libri as NumriLibrit,id_student as Studenti, data_marrje, data_kthimi FROM huazimet";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+        }
         public Form4()
         {
             InitializeComponent();
@@ -19,7 +32,31 @@ namespace LogIn
 
         private void button1_Click(object sender, EventArgs e)
         {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                string query = "INSERT INTO huazimet (id_libri,id_student,data_marrje,data_kthimi)VALUES(@id_libri,@id_student,@data_marrje,@data_kthimi)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id_libri", textBox1.Text);
+                cmd.Parameters.AddWithValue("@id_student", textBox2.Text);
+                cmd.Parameters.AddWithValue("@data_marrje", dateTimePicker1);
+                cmd.Parameters.AddWithValue("@data_kthimi", dateTimePicker2);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                Loadbibloteka();
+            }
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id"].Value);
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                     
+                }
+            }
         }
     }
 }
