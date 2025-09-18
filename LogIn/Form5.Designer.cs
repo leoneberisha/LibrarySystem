a0b87e25aa5dfb766dd563d65a2e363be91a3d08
leoneberisha <leonaberiisha@gmail.com>
2025-09-18 23:41:07 +0200

    .

--- LogIn/Form5.Designer.cs
+++ LogIn/Form5.Designer.cs
@@ -136,6 +136,7 @@
             this.button3.TabIndex = 6;
             this.button3.Text = "Fshije";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -144,8 +145,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 32);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Update";
+            this.button2.Text = "Edit";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
